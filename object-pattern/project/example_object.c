86ee8acb0d459a0a530aaa0a85003b710c948f69
Henrik Samuelsson <henrik.samuelsson2@saabgroup.com>
2023-03-27 09:20:32 +0200

    Start on example_object.c
    Currently some compilation problem to be resolved.

---
+++ object-pattern/project/example_object.c
@@ -0,0 +1,13 @@
+#include "example_object.h"
+
+#include <string.h>
+
+void example_object_init(struct example_object *self)
+{
+    memset_s(self, sizeof(*self), 0, sizeof(*self));
+}
+
+void example_deinit(struct example_object *self)
+{
+    /* Clean up... */
+}
